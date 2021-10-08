22ba6d5233a4eddaa7f23fe0402af6c112128cf5
Anukarsh <anukarshr@gmail.com>
2021-10-08 10:39:53 +0530

    update

--- to-do-list/src/myComponents/Todos.js
+++ to-do-list/src/myComponents/Todos.js
@@ -4,9 +4,10 @@ import {TodoItem} from '../myComponents/TodoItem'
 export const Todos = (props) => {
     return (
         <div className="container">
-            <h3 className="text-center">To Do's List</h3>
-            {/* {props.todos} */}
-            <TodoItem todo={props.todos[1]}/>
+            <h3 className="text-center my-3">To Do's List</h3>
+            {props.todos.map((todo)=>{
+                return <TodoItem todo={todo} onDelete={onDelete}/>
+            })}
         </div>
     )
 }
